e695a6c7b039f29c37d628cd21cccbd76fec23e1
Saleh Ramadan <salehram@users.noreply.github.com>
2023-04-29 18:21:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     someVarFromEnv = os.environ.get('someVar')
-    return 'This is a testing app.\nMy environment variable is: {}!\n'.format(someVarFromEnv)
+    return 'I have changed the text here for testing: {}!\n'.format(someVarFromEnv)
 
 if __name__ == "__main__":
     app.debug = True
